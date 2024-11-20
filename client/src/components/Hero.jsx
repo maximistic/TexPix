ffdc32306c332a827a98a3c2d73f21907ff79e18
maximistic <110153672+maximistic@users.noreply.github.com>
2024-11-20 15:19:19 +0530

    hero final update

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,34 +1,46 @@
-import { assets } from "../assets/assets"
+import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
-    <div className="felx flex-col justify-center items-center text-center my-20">
-        <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
-            <p>Try out our Text to Image Generator</p>
-            <img src={assets.star_icon} alt="" />
-        </div>
+    <div className="flex flex-col justify-center items-center text-center my-20 px-4">
 
-        <h1 className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center"> Turn Text into <span className="text-blue-600">pics</span>, in seconds!</h1>
+      <div className="text-neutral-700 inline-flex items-center gap-3 bg-gray-100 px-6 py-2 rounded-full border border-neutral-300 shadow-md hover:shadow-lg transition-shadow duration-200">
+        <p className="font-medium">Try out our Text to Image Generator</p>
+        <img src={assets.star_icon} alt="star icon" className="w-5 h-5" />
+      </div>
 
-        <p className="text-center max-w-xl mx-auto mt-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, reprehenderit nam! Voluptatum nisi optio id reiciendis deserunt quod, sint ipsum, dignissimos, tenetur pariatur incidunt tempore in error impedit. Sequi, modi!</p>
-    
-        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
-            Generate Images
-            <img className="h-6" src={assets.star_group} alt="" />
-        </button>
+      <h1 className="text-4xl sm:text-7xl font-extrabold max-w-[300px] sm:max-w-[590px] mx-auto mt-10">
+        Turn Text into{" "}
+        <span className="text-blue-600 underline decoration-2">pics</span>, in seconds!
+      </h1>
 
-        <div className="flex flex-wrap justify-center mt-16 gap-3">
-          {Array(6).fill('').map((item, index)=>(
-            <img className="rounded hover:scale-110 transition-all duration-300 cursor-pointer max-sm:w-10" 
-            src={index % 2 == 0 ? assets.sample_img_1 : assets.sample_img_2} 
-            alt="" key={index} width={70}
+      <p className="text-neutral-600 text-center max-w-xl mx-auto mt-5 text-sm sm:text-base">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, reprehenderit nam!
+        Voluptatum nisi optio id reiciendis deserunt quod, sint ipsum, dignissimos, tenetur
+        pariatur incidunt tempore in error impedit. Sequi, modi!
+      </p>
+
+      <button className="text-base sm:text-lg text-white bg-black mt-8 px-10 py-3 flex items-center gap-3 rounded-full hover:bg-neutral-800 transition-colors duration-200">
+        Generate Images
+        <img src={assets.star_group} alt="star group icon" className="h-5 sm:h-6" />
+      </button>
+
+      <div className="flex flex-wrap justify-center mt-16 gap-4">
+        {Array(6)
+          .fill("")
+          .map((item, index) => (
+            <img
+              className="rounded-lg hover:scale-110 transition-transform duration-300 cursor-pointer w-16 sm:w-20"
+              src={index % 2 === 0 ? assets.sample_img_1 : assets.sample_img_2}
+              alt="sample"
+              key={index}
             />
           ))}
-        </div>
+      </div>
 
-        <p className="mt-2 text-neutral-600">Sample images from TEXPIX</p>
+      <p className="mt-4 text-neutral-500 text-sm">Sample images from TEXPIX</p>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero"
+import Howto from "../components/Howto"
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Howto />
     </div>
   )
 }
