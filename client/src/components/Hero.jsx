b1b3f9d40eac176f49b8179905a55911b7ebbd7a
maximistic <110153672+maximistic@users.noreply.github.com>
2024-11-20 14:57:27 +0530

    update hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -12,10 +12,21 @@ const Hero = () => {
 
         <p className="text-center max-w-xl mx-auto mt-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, reprehenderit nam! Voluptatum nisi optio id reiciendis deserunt quod, sint ipsum, dignissimos, tenetur pariatur incidunt tempore in error impedit. Sequi, modi!</p>
     
-        <button className="">
+        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
             Generate Images
-            <img src={assets.star_group} alt="" />
+            <img className="h-6" src={assets.star_group} alt="" />
         </button>
+
+        <div className="flex flex-wrap justify-center mt-16 gap-3">
+          {Array(6).fill('').map((item, index)=>(
+            <img className="rounded hover:scale-110 transition-all duration-300 cursor-pointer max-sm:w-10" 
+            src={index % 2 == 0 ? assets.sample_img_1 : assets.sample_img_2} 
+            alt="" key={index} width={70}
+            />
+          ))}
+        </div>
+
+        <p className="mt-2 text-neutral-600">Sample images from TEXPIX</p>
     </div>
   )
 }
