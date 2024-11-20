179cf2712007b3db2ca43f9a834c6ad284de4a3f
maximistic <110153672+maximistic@users.noreply.github.com>
2024-11-20 16:56:55 +0530

    testimonials creation

---
+++ client/src/components/Testimonials.jsx
@@ -0,0 +1,11 @@
+
+const Testimonials = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-20 py-12">
+        <h1 className="text-3xl sm:text-4xl font-semibold mb-2">Create Images</h1>
+        <p className="text-gray-500 mb-8">Turn your thoughts into visuals</p>
+    </div>
+  )
+}
+
+export default Testimonials
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import Desc from "../components/Desc"
 import Hero from "../components/Hero"
 import Howto from "../components/Howto"
+import Testimonials from "../components/Testimonials"
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Howto />
+      <Desc />
+      <Testimonials />
     </div>
   )
 }
