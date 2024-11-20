13332df0680698375bd57def7fbe04102c86f9ce
maximistic <110153672+maximistic@users.noreply.github.com>
2024-11-20 18:16:46 +0530

    footer.jsx update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import BuyCredits from './pages/BuyCredits'
 import Generator from './pages/Generator'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
       <Navbar />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/buy' element={<BuyCredits />} />
         <Route path='/generator' element={<Generator />} />
       </Routes>
       
+      <Footer />
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import Desc from "../components/Desc"
+import Generatebtn from "../components/Generatebtn"
 import Hero from "../components/Hero"
 import Howto from "../components/Howto"
 import Testimonials from "../components/Testimonials"
 
 const Home = () => {
   return (
+
     <div>
       <Hero />
       <Howto />
       <Desc />
       <Testimonials />
+      <Generatebtn />
     </div>
+    
   )
 }
 
